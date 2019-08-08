5c3d06ee11fdc2f152952c7c0ec4c8d358f37b8c
Santeri Hurnanen <santeri@oikeuttaelaimille.fi>
2019-08-09 02:11:06 +0300

    fix: improve build tools

--- tools/prepare.sh
+++ tools/prepare.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 
+# Exit if any command fails.
+set -o pipefail
+trap 'exit' ERR
+
+# Include helper library
+. ./tools/utils.sh
+
 VERSION="$1"
 BRANCH="$2"
-COMMITS="$3"
-TIME="$4"
 
-# tmp=$(mktemp)
-# Replace version number in sfdx-project.json.
-# jq '.packageDirectories[].versionNumber = "${VERSION}"' sfdx-project.json >> "$tmp" && mv "$tmp" sfdx-project.json
+PACKAGE_NAME=$(get_default_package_name)
+
+# If found package name with current scope.
+if [ ! -z "$PACKAGE_NAME" ]; then
+	echo "Releasing $PACKAGE_NAME@$VERSION.NEXT"
+
+	# Create new package version.
+	PACKAGE=$(sfdx force:package:version:create -p "$PACKAGE_NAME" --installationkeybypass -w 20 -n "$VERSION.NEXT" -t "v$VERSION" -b "$BRANCH" --json)
+	STATUS="$(echo $PACKAGE | jq '.status')"
+
+	echo "$PACKAGE"
+
+	if [ -z "$STATUS" ] || [ "$STATUS" -gt 0 ]; then
+		exit 1
+	fi
+
+	# 04t... id of package version.
+	PACKAGE_VERSION="$(echo $PACKAGE | jq -r '.result.SubscriberPackageVersionId')"
+fi
--- tools/publish.sh
+++ tools/publish.sh
@@ -1,38 +1,30 @@
 #!/usr/bin/env bash
 
-PACKAGE_NAME="$(jq -r '.packageDirectories[] | select(.default==true) | .package' sfdx-project.json)"
+# Exit if any command fails.
+set -o pipefail
+trap 'exit' ERR
+
+# Include helper library
+. ./tools/utils.sh
+
 VERSION="$1"
 BRANCH="$2"
-COMMITS="$3"
-TIME="$4"
 
-SFDX_CLI_EXEC=sfdx
+PACKAGE_NAME=$(get_default_package_name)
+PACKAGE_VERSION=$(get_package_version "$PACKAGE_NAME" "$VERSION")
 
-# Defining Salesforce CLI exec, depending if it's CI or local dev machine
-if [ $CI ]; then
-    # echo "Script is running on CI"
-    SFDX_CLI_EXEC="sfdx"
+if [ ! -z "$PACKAGE_VERSION" ]; then
+	echo "Promoting $PACKAGE_VERSION to released" >&2
+
+	# Promote the package version to released
+	RELEASE=$(sfdx force:package:version:promote -p "$PACKAGE_VERSION" --json --noprompt)
+	STATUS="$(echo $RELEASE | jq '.status')"
+
+	echo "$RELEASE" >&2
+
+	if [ -z "$STATUS" ] || [ "$STATUS" -gt 0 ]; then
+		exit 1
+	fi
+
+	echo "{ \"name\": \"$PACKAGE_NAME\", \"url\": \"https://login.salesforce.com/packaging/installPackage.apexp?p0=$PACKAGE_VERSION\" }"
 fi
-
-PACKAGE=$($SFDX_CLI_EXEC force:package:version:create -p "$PACKAGE_NAME" --installationkeybypass -w 20 -n "$VERSION.NEXT" -a "$VERSION-$BRANCH" -t "v$VERSION" -b "$BRANCH" --json)
-STATUS="$(echo $PACKAGE | jq '.status')"
-
-if [ -z "$STATUS" ] || [ "$STATUS" -gt 0 ]; then
-    echo "$PACKAGE"
-    exit 1
-fi
-
-PACKAGE_VERSION="$(echo $PACKAGE | jq -r '.result.SubscriberPackageVersionId')"
-
-# Only promote master branch.
-RELEASE=$($SFDX_CLI_EXEC force:package:version:promote -p "$PACKAGE_VERSION" --json --noprompt)
-STATUS="$(echo $PACKAGE | jq '.status')"
-
-if [ -z "$STATUS" ] || [ "$STATUS" -gt 0 ]; then
-    echo "$STATUS"
-    exit 1
-fi
-
-RELEASE_ID="$(echo $RELEASE | jq -r '.result.id')"
-
-echo "{ \"name\": \"$PACKAGE_NAME\", \"url\": \"https://login.salesforce.com/packaging/installPackage.apexp?p0=$RELEASE_ID\" }"
